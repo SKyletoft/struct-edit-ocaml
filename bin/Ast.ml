a3302972db0edd0ae66bc971e6a021987668cbc0
Samuel Kyletoft <samuel@kyletoft.se>
2023-05-20 15:02:38 +0200

    ocamlformat: Update formatting settings for airier code

--- bin/Ast.ml
+++ bin/Ast.ml
@@ -56,7 +56,8 @@ and func = {
 let indent = Printf.sprintf "\t%s"
 let indent_ = List.map indent
 
-let rec ts_argument { arg; ty } = Printf.sprintf "%s: %s" arg ty
+let rec ts_argument { arg; ty } =
+  Printf.sprintf "%s: %s" arg ty
 
 and ts_bop = function
   | LT -> "<"
@@ -78,7 +79,9 @@ and ts_bop = function
   | BOr -> "|"
   | BAnd -> "&"
 
-and ts_uop = function Not -> "!" | BNot -> "~"
+and ts_uop = function
+  | Not -> "!"
+  | BNot -> "~"
 
 and ts_expr = function
   | BinOps (e, []) -> ts_expr e
@@ -97,7 +100,9 @@ and ts_expr = function
   | Bool false -> "false"
   | Call { func; args } ->
       let name = ts_expr func in
-      args |> List.map ts_expr |> String.concat ", "
+      args
+      |> List.map ts_expr
+      |> String.concat ", "
       |> Printf.sprintf "%s(%s)" name
   | Object _ -> ""
   | Function { arguments = _; return_type = _; body = _ } -> ""
@@ -109,7 +114,10 @@ and ts_statement = function
   | If (cond, { block = ss }, None) ->
       let cond_s = ts_expr cond in
       let then_s =
-        ss |> List.map ts_statement |> List.flatten |> List.map indent
+        ss
+        |> List.map ts_statement
+        |> List.flatten
+        |> List.map indent
       in
       let c = Printf.sprintf "if (%s) {" cond_s in
       let e = "}" in
@@ -117,31 +125,65 @@ and ts_statement = function
   | If (cond, { block = t }, Some { block = e }) ->
       let first =
         If (cond, { block = t }, None)
-        |> ts_statement |> List.rev (* Drop the last element *) |> List.tl
+        |> ts_statement
+        (* Drop the last element *)
+        |> List.rev
+        |> List.tl
         |> List.rev
       in
       let else_ =
-        e |> List.map ts_statement |> List.flatten |> List.map indent
+        e
+        |> List.map ts_statement
+        |> List.flatten
+        |> List.map indent
       in
-      List.append (List.append first ("} else {" :: else_)) [ "}" ]
+      List.append
+        (List.append first ("} else {" :: else_))
+        [ "}" ]
   | Let (name, ty, Some e) ->
       let e = ts_expr e in
-      let t = match ty with Some t -> Printf.sprintf ": %s" t | None -> "" in
+      let t =
+        match ty with
+        | Some t -> Printf.sprintf ": %s" t
+        | None -> ""
+      in
       [ Printf.sprintf "let %s%s = %s;" name t e ]
   | Let (name, ty, None) ->
-      let t = match ty with Some t -> Printf.sprintf ": %s" t | None -> "" in
+      let t =
+        match ty with
+        | Some t -> Printf.sprintf ": %s" t
+        | None -> ""
+      in
       [ Printf.sprintf "let %s%s;" name t ]
   | Const (name, ty, e) ->
       let e = ts_expr e in
-      let t = match ty with Some t -> Printf.sprintf ": %s" t | None -> "" in
+      let t =
+        match ty with
+        | Some t -> Printf.sprintf ": %s" t
+        | None -> ""
+      in
       [ Printf.sprintf "let %s%s = %s;" name t e ]
 
-and ts_func { name; arguments; return_type; body = { block = body } } =
-  let args_s = arguments |> List.map ts_argument |> String.concat ", " in
-  let body_s =
-    body |> List.map ts_statement |> List.flatten |> List.map indent
+and ts_func
+    {
+      name;
+      arguments;
+      return_type;
+      body = { block = body };
+    } =
+  let args_s =
+    arguments |> List.map ts_argument |> String.concat ", "
   in
-  let first = Printf.sprintf "function %s(%s): %s {" name args_s return_type in
+  let body_s =
+    body
+    |> List.map ts_statement
+    |> List.flatten
+    |> List.map indent
+  in
+  let first =
+    Printf.sprintf "function %s(%s): %s {" name args_s
+      return_type
+  in
   List.append (first :: body_s) [ "}" ]
 
 (*
@@ -178,7 +220,13 @@ let fib : func =
                               {
                                 func = Ident "fib";
                                 args =
-                                  [ BinOps (Ident "n", [ (Sub, Number "2") ]) ];
+                                  [
+                                    BinOps
+                                      ( Ident "n",
+                                        [
+                                          (Sub, Number "2");
+                                        ] );
+                                  ];
                               } );
                         Const
                           ( "before_that",
@@ -187,10 +235,20 @@ let fib : func =
                               {
                                 func = Ident "fib";
                                 args =
-                                  [ BinOps (Ident "n", [ (Sub, Number "1") ]) ];
+                                  [
+                                    BinOps
+                                      ( Ident "n",
+                                        [
+                                          (Sub, Number "1");
+                                        ] );
+                                  ];
                               } );
                         Return
-                          (BinOps (Ident "prev", [ (Add, Ident "before_that") ]));
+                          (BinOps
+                             ( Ident "prev",
+                               [
+                                 (Add, Ident "before_that");
+                               ] ));
                       ];
                   } );
           ];
