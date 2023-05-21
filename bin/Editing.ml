4a429a4c6a97db6367763abc3736fcc13d4a06f2
Samuel Kyletoft <samuel@kyletoft.se>
2023-05-21 04:21:28 +0200

    bin/Editing, Main: Add insert functions
    to add a function, then name it and then add a return type

--- bin/Editing.ml
+++ bin/Editing.ml
@@ -1,1 +1,27 @@
 open Ast
+
+let insert_function tree =
+  List.append tree
+    [
+      Func
+        {
+          name = None;
+          arguments = [];
+          return_type = None;
+          body = { block = [] };
+        };
+    ]
+
+let insert_decl_name d n =
+  match d with
+  | Func { name = _; arguments; return_type; body } ->
+      Func { name = Some n; arguments; return_type; body }
+  | Let (_, ty, value) -> Let (Some n, ty, value)
+  | Const (_, ty, value) -> Const (Some n, ty, value)
+
+let insert_decl_type d t =
+  match d with
+  | Func { name; arguments; return_type = _; body } ->
+      Func { name; arguments; return_type = Some t; body }
+  | Let (name, _, value) -> Let (name, Some t, value)
+  | Const (name, _, value) -> Const (name, Some t, value)
--- bin/Main.ml
+++ bin/Main.ml
@@ -3,3 +3,12 @@ open Editing
 
 let () =
   fib |> ts_decl |> String.concat "\n" |> print_endline;
+
+  let t = ref [] in
+  t := insert_function !t;
+
+  !t
+  |> List.map ts_decl
+  |> List.flatten
+  |> String.concat "\n"
+  |> print_endline
