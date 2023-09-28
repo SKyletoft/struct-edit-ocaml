190a9d29770659c55a6dae86ff75635c83efd299
Samuel Kyletoft <samuel@kyletoft.se>
2023-09-28 13:49:31 +0200

    Initial backwards movement impl

--- rust-ver/src/main.rs
+++ rust-ver/src/main.rs
@@ -1,6 +1,7 @@
-use std::io::stdout;
+use std::{io::stdout, ops::Range};
 
 use crossterm::{
+	cursor::MoveTo,
 	event::{self, read, KeyCode, KeyEvent},
 	style::*,
 	terminal::{Clear, ClearType},
@@ -12,9 +13,76 @@ extern "C" {
 	fn tree_sitter_typescript() -> Language;
 }
 
-struct Cursor<'a> {
+type MovementRes = Result<(), ()>;
+
+pub struct Cursor<'a> {
 	ts_cur: tree_sitter::TreeCursor<'a>,
 	cursor_stack: Vec<u8>,
+}
+
+impl<'a> Cursor<'a> {
+	pub fn new(t: &'a tree_sitter::Tree) -> Self {
+		Cursor {
+			ts_cur: t.root_node().walk(),
+			cursor_stack: Default::default(),
+		}
+	}
+
+	pub fn up(&mut self) -> MovementRes {
+		if self.ts_cur.goto_parent() {
+			self.cursor_stack
+				.pop()
+				.expect("Should be in sync with ts_cur");
+		}
+		Ok(())
+	}
+
+	pub fn down(&mut self) -> MovementRes {
+		if self.ts_cur.goto_first_child() {
+			self.cursor_stack.push(0);
+		}
+		Ok(())
+	}
+
+	pub fn forwards(&mut self) -> MovementRes {
+		if self.ts_cur.goto_next_sibling() {
+			*self.cursor_stack.last_mut().unwrap() += 1;
+		} else {
+			self.up()?;
+			self.forwards()?;
+			self.down()?;
+		}
+		Ok(())
+	}
+
+	pub fn backwards(&mut self) -> MovementRes {
+		match self.cursor_stack.last().copied() {
+			None => Err(()),
+			Some(0) => {
+				self.up()?;
+				self.backwards()?;
+				self.down()?;
+				let mut steps = 0;
+				while self.ts_cur.goto_next_sibling() {
+					steps += 1;
+				}
+				*self.cursor_stack.last_mut().unwrap() = steps;
+				Ok(())
+			}
+			Some(n) => {
+				self.up()?;
+				self.down()?;
+				for _ in 0..(n - 1) {
+					self.forwards()?;
+				}
+				Ok(())
+			}
+		}
+	}
+
+	pub fn byte_range(&self) -> Range<usize> {
+		self.ts_cur.node().byte_range()
+	}
 }
 
 fn run() -> std::io::Result<()> {
@@ -25,13 +93,10 @@ fn run() -> std::io::Result<()> {
 	let source = include_str!("example.ts");
 
 	let out = parser.parse(source, None).unwrap();
-	let root = out.root_node();
-
-	let mut cur = out.walk();
-	cur.goto_first_child();
+	let mut cur = Cursor::new(&out);
 
 	loop {
-		let range = cur.node().byte_range();
+		let range = cur.byte_range();
 		let start = range.start;
 		let end = range.end;
 		let before = &source[..start];
@@ -40,7 +105,9 @@ fn run() -> std::io::Result<()> {
 
 		let stdout = stdout();
 		let mut lock = stdout.lock();
-		lock.execute(ResetColor)?.execute(Clear(ClearType::All))?;
+		lock.execute(ResetColor)?
+			.execute(Clear(ClearType::All))?
+			.execute(MoveTo(1, 1))?;
 
 		for line in before.lines() {
 			lock.execute(Print("\n\r"))?.execute(Print(line))?;
@@ -59,19 +126,25 @@ fn run() -> std::io::Result<()> {
 				code: KeyCode::Char('d'),
 				..
 			}) => {
-				cur.goto_next_sibling();
+				let _ = cur.forwards();
+			}
+			event::Event::Key(KeyEvent {
+				code: KeyCode::Char('a'),
+				..
+			}) => {
+				let _ = cur.backwards();
 			}
 			event::Event::Key(KeyEvent {
 				code: KeyCode::Char('s'),
 				..
 			}) => {
-				cur.goto_first_child();
+				let _ = cur.down();
 			}
 			event::Event::Key(KeyEvent {
 				code: KeyCode::Char('w'),
 				..
 			}) => {
-				cur.goto_parent();
+				let _ = cur.up();
 			}
 			event::Event::Key(KeyEvent {
 				code: KeyCode::Char('q') | KeyCode::Esc,
